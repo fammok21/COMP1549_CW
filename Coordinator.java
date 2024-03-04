7544de583328a055cfd593ae73864c0634fd15aa
fammok21 <149542653+fammok21@users.noreply.github.com>
2024-03-04 11:08:59 +0000

    Add files via upload

---
+++ Coordinator.java
@@ -0,0 +1,27 @@
+package week3;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Coordinator{
+    String name;
+    //ArrayList<String> clientDetails = new ArrayList<>();
+
+    public Coordinator(String name) {
+    	this.name = name;
+    }
+
+    public ArrayList<String> showDetails(ArrayList<String> name) {
+    	return name;
+    }
+    
+  
+    }    
+    
+
+
+
+
+
