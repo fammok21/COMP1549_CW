5d9dad13ddbe8be8a72e397a1ebfab6e7a174e88
fammok21 <149542653+fammok21@users.noreply.github.com>
2024-03-11 15:50:09 +0000

    Update Coordinator.java

--- Coordinator.java
+++ Coordinator.java
@@ -4,24 +4,33 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
+import java.util.concurrent.TimeUnit;
 public class Coordinator{
-    String name;
-    //ArrayList<String> clientDetails = new ArrayList<>();
+    User user;
 
-    public Coordinator(String name) {
-    	this.name = name;
-    }
-
-    public ArrayList<String> showDetails(ArrayList<String> name) {
-    	return name;
+    public Coordinator(User user) {
+    	this.user = user;
     }
     
-  
-    }    
+    public String returnCoordinator() {
+    	return user.getUsername() + user.getServerAddress() + user.getPort();
+    }
     
+    
+    public void checkClients(List<String> names) {
+    	while (true) {
+            // Perform the necessary checks on the active group members
+            // For example, you can print the current state of active group members
+            System.out.println("Active Group Members: " + names);
 
-
-
-
-
+            try {
+                // Sleep for 20 seconds before the next check
+                TimeUnit.SECONDS.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }}
+    
+    public List<String> returnUserDetails(List<String> names){
+    	return names;
+    }}
